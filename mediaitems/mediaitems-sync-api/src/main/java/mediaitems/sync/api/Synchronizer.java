1eae38ab62a7f2146f7fd21a64f34f897807b259
lm <mueller.lutz@gmail.com>
2013-12-14 12:37:19 +0100

    fix parent project eclipse setup

--- mediaitems/mediaitems-sync-api/src/main/java/mediaitems/sync/api/Synchronizer.java
+++ mediaitems/mediaitems-sync-api/src/main/java/mediaitems/sync/api/Synchronizer.java
@@ -1,12 +1,24 @@
 package mediaitems.sync.api;
 
 import mediaitems.metadata.domain.MediaItem;
+import mediaitems.metadata.repository.MediaItemRepository;
 import mediaitems.sources.api.ContentHandle;
+import mediaitems.sources.api.ContentSource;
 
 public interface Synchronizer {
-			public interface SynchronizerCallback {
-				public void execute(MediaItem mediaItem,ContentHandle contentHandle);
-			}
+	
+	public interface Event {
+		public MediaItem getMediaItem();
+		public ContentHandle getContentHandle();
+	}
 
-			public void execute(SynchronizerCallback onNewItem,SynchronizerCallback onChangedItem,SynchronizerCallback onDeletedItem);
+	public interface EventHandler {
+		public void onEvent(Event event);
+	}
+
+	public void registerEventHandler(String eventType,EventHandler eventHandler,short priority);
+	
+	public void synchronize(ContentSource contentSource,MediaItemRepository mediaItemRepository);
+	
+	
 }
