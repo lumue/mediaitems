cc34c5011de72e1014ae2d90735b42e43909b6d2
lm <mueller.lutz@gmail.com>
2013-12-17 23:18:24 +0100

    dart webclient in webapp integrieren

--- mediaitems/mediaitems-app/src/main/java/mediaitems/app/http/Application.java
+++ mediaitems/mediaitems-app/src/main/java/mediaitems/app/http/Application.java
@@ -2,14 +2,16 @@ package mediaitems.app.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.socket.server.config.EnableWebSocket;
 
 @Configuration
 @EnableAutoConfiguration
@@ -17,14 +19,23 @@ import org.springframework.web.socket.server.config.EnableWebSocket;
 		"mediaitems.http" })
 @EnableMongoRepositories
 @EnableWebMvc
-@EnableWebSocket
 @EnableTransactionManagement
 public class Application extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ServletRegistrationBean dispatcherRegistration(
+			DispatcherServlet dispatcherServlet) {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				dispatcherServlet);
+		registration.addUrlMappings("/app/*");
+		return registration;
+	}
 
 	@Override
 	public void configureDefaultServletHandling(
 			DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+
 	}
 
 	public static void main(String[] args) {
