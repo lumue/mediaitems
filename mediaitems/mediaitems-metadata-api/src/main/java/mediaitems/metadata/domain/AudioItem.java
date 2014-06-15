da10982f1f03a53ef81d5bd6aeb0c19eedbec76f
lm <mueller.lutz@gmail.com>
2014-06-15 22:40:43 +0200

    mediaitems-http ist jetz mediaitems-application mediaitems-app ist jetzt mediaitems-webapp

--- mediaitems/mediaitems-metadata-api/src/main/java/mediaitems/metadata/domain/AudioItem.java
+++ mediaitems/mediaitems-metadata-api/src/main/java/mediaitems/metadata/domain/AudioItem.java
@@ -7,13 +7,11 @@ public interface AudioItem extends MediaItem {
 	public interface AudioItemBuilder<T extends AudioItem> extends
 			MediaItemBuilder<T> {
 		
-
 		public AudioItemBuilder<T> setAudioCodec(String codec);
 
 		public AudioItemBuilder<T> setLength(Duration duration);
 		
 	}
-
 	public String getAudioCodec();
 
 	public Duration getLength();
