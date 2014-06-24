8b88195ad2e1735a51f1e4cb4a8dcdda3880889d
lm <mueller.lutz@gmail.com>
2014-06-24 13:16:51 +0200

    fix spring mvc initialization and json response handling

--- mediaitems/mediaitems-webapp/src/main/java/mediaitems/app/http/Application.java
+++ mediaitems/mediaitems-webapp/src/main/java/mediaitems/app/http/Application.java
@@ -5,18 +5,15 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @EnableMongoRepositories(basePackages = { "mediaitems.metadata.springdata.repository" })
 @ComponentScan(basePackages = { "mediaitems.app.http",
 		"mediaitems.app.integration", "mediaitems.http",
 		"mediaitems.metadata.springdata" })
-@EnableWebMvc
 @EnableTransactionManagement
 @EnableIntegration
 @IntegrationComponentScan
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 }
